c500d8b938671ab319ddb4e918d57c23b3ca8b99
ashucodejs <simplexpersu@gmail.com>
2025-05-11 06:55:00 +0000

    data type summary

--- .devcontainer/03_conversionOperation.js
+++ .devcontainer/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = 34
+/*let score = 34
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 //"33 =>33"
 //"33abc" => NaN
@@ -23,5 +23,41 @@ let someNumber = 33
 
 let stringNumber = string(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringnumber);
+//console.log(stringNumber)
+//console.log(typeof stringnumber);
+
+//***********operations **************************** 
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+
+let str1 = "hello"
+let str2 = " ashu"
+
+let str3 = str1 + str2
+console.log(str3);
+/*
+console.log("1"+2)//12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"3") // 32*/
+
+//console.log(+true)
+//console.log(+"")
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter**;
+console.log(gameCounter)
---
+++ .devcontainer/datatype-summary.js
@@ -0,0 +1,33 @@
+// premitive
+
+//7 : string , number , boolean , null  ,undefined , symbol , bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id= symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 7890890890890n
+
+// refrence ( non premitive)
+
+// arry , obeject , fuction
+
+const hero = ["sasktiman " , "naagraj" , "doga"]
+let myObj = {
+    name: "ashu"
+    age : 23;
+
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
